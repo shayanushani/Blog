fe3797837f2d8abbf9323abc1ffbc447c5b065c2
tparnell <tparnell@cargurus.com>
2019-04-14 12:48:11 -0400

    switch out icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         background_color: `#00a0dd`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/favicon.ico`,
       },
     },
     `gatsby-plugin-offline`,
