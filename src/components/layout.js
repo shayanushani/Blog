31bde32ac4b068beeb27d4aca1f2f8ae718a2cab
tparnell <tparnell@cargurus.com>
2019-04-17 14:49:39 -0400

    add code owner for content

--- src/components/layout.js
+++ src/components/layout.js
@@ -27,7 +27,7 @@ class Layout extends React.Component {
             {new Date().getFullYear()}
 ,
             {' '}
-            <a href="www.cargurus.com">CarGurus</a>
+            <a href="https://www.cargurus.com">CarGurus</a>
           </footer>
         </div>
       </Fragment>
