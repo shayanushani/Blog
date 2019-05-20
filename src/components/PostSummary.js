33a8604f09f2bba75df1bbf7b82fdc6498732ebb
tparnell <tparnell@cargurus.com>
2019-05-20 12:00:41 -0400

    #41 adjust header link style

--- src/components/PostSummary.js
+++ src/components/PostSummary.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { rhythm } from '../utils/typography';
 
 import styles from './PostSummary.module.less';
 
 export default function PostSummary({ slug, description, title, date }) {
   return (
     <article key={slug}>
-      <h3
-        style={{
-          marginBottom: rhythm(1 / 4),
-        }}
-      >
+      <h3 className={styles.h3Style}>
         <Link style={{ boxShadow: 'none' }} to={slug}>
           {title}
         </Link>
